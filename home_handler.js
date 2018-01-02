87914851eba1344010e39286266d2de908aae3f1
Rodrigo Araujo <rodemfa13@gmail.com>
2018-01-02 11:42:37 -0200

    Delete home_handler.js

--- home_handler.js
+++
@@ -1,6 +0,0 @@
-class HomeHandler {
-	constructor() { this.homeStyle = document.getElementById("home").style; }
-
-	close() { this.homeStyle.display = "none"; }
-	open() { this.homeStyle.display = "block"; }
-}
